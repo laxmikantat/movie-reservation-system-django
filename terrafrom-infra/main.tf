278420ab3f9b4b2fa9b73c23eddded77c8881011
Laxmikant Tamboli <laxmikanttamboli0@gmail.com>
2026-05-17 17:52:11 +0530

    Fixed terraform formatting

--- terrafrom-infra/main.tf
+++ terrafrom-infra/main.tf
@@ -1,9 +1,9 @@
 module "vpc" {
   source = "./modules/vpc"
 
-  vpc_cidr            = var.vpc_cidr
-  public_subnet_1     = var.subnet_1
-  public_subnet_2     = var.subnet_2
+  vpc_cidr        = var.vpc_cidr
+  public_subnet_1 = var.subnet_1
+  public_subnet_2 = var.subnet_2
 
   availability_zone_1 = "us-east-1a"
   availability_zone_2 = "us-east-1b"
@@ -27,7 +27,7 @@ module "ec2" {
   subnet_id         = module.vpc.subnet_1_id
   security_group_id = module.security_group.security_group_id
   key_name          = aws_key_pair.deployer.key_name
-  
+
   # FIX: Removed the stray "$" and wrapped in "${path.module}"
   user_data = file("${path.module}/scripts/script.sh")
 
