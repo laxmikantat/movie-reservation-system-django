5365e9d386c6b1648f65caadd718b49c1c9d6e35
Laxmikant Tamboli <laxmikanttamboli0@gmail.com>
2026-05-17 17:57:04 +0530

    Fixed terraform recursive formatting

--- terrafrom-infra/environments/qa/terraform.tfvars
+++ terrafrom-infra/environments/qa/terraform.tfvars
@@ -1,7 +1,7 @@
-vpc_cidr     = "10.0.0.0/16"
+vpc_cidr = "10.0.0.0/16"
 
-subnet_1     = "10.0.1.0/24"
-subnet_2     = "10.0.2.0/24"
+subnet_1 = "10.0.1.0/24"
+subnet_2 = "10.0.2.0/24"
 
-ami_id       = "ami-091138d0f0d41ff90"
+ami_id        = "ami-091138d0f0d41ff90"
 instance_type = "t2.micro"
--- terrafrom-infra/modules/ec2/main.tf
+++ terrafrom-infra/modules/ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web_server" {
-  ami                    = var.ami_id
-  instance_type          = var.instance_type
-  subnet_id              = var.subnet_id
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
 
   vpc_security_group_ids = [var.security_group_id]
 
